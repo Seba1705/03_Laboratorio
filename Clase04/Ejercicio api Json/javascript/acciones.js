6fabbbd9d4e1ae358d59e7bc30a8e152d88fff33
Sebastian1705 <sebaaguirre2012@gmail.com>
2019-04-17 23:39:36 -0300

    ejercicio completo, base en node leida y tabla escrita

--- Clase04/Ejercicio api Json/javascript/acciones.js
+++ Clase04/Ejercicio api Json/javascript/acciones.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function(){         //Funcion anonima
-    var btnAgregar = $("btnGuardar");
+    var btnAgregar = $("btn");
     btnAgregar.addEventListener("click", agregar);
 })
 
@@ -15,16 +15,26 @@ function agregar(){
     var date;
     var phone;
     var peticion;
-    var localhost = ""
+    var localhost = "http://localhost:3000/personas";
     var metodo = "GET";
-
+    var body = $("tbody")
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = callback;
-    xhttp.open(metodo, localhost, true)
-
+    xhttp.open("GET", localhost, true)
+    xhttp.send();
     function callback(){
         if(xhttp.readyState === 4 && xhttp.status === 200)
             console.log("Llego la repuesta del servidor");
+        var resp = xhttp.responseText;
+        console.log(resp);
+        var obj = JSON.parse(resp);
+        for(var i=0; i<Object.keys(obj).length;i++ ){
+            name = obj[i].nombre;
+            lastname = obj[i].apellido;
+            date = obj[i].fecha;
+            phone = obj[i].fecha;
+            body.innerHTML += "<tr><td>"+ name+"</td>"+"<td>"+ lastname+"</td>"+"<td>"+ date+"</td>"+"<td>"+ phone+"</td>"+"<td>"+"<a href=''>borrar</a>" + "</td></tr>";
+        }   
     }   
 }
