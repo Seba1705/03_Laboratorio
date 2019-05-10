7e5f7f0aedda4eb48464565cbbd5bcf393bbad27
Sebastian1705 <sebaaguirre2012@gmail.com>
2019-05-09 22:14:44 -0300

    Modelo parcial completo, spinner echo solo con css, sin imagen

--- Clases/Clase04/js/js.js
+++ Clases/Clase04/js/js.js
@@ -1,57 +1,38 @@
-// //crear variable
-// var xhttp = new XMLHttpRequest();
-// //Escuchamos la respuesta pasando un callback
-// xhttp.onreadystatechange = callback;
-// //open - abrir coneccion con esa ruta
-// xhttp.open("GET","http://192.168.2.28:3000/loginUsuario",true);
-// xhttp.send();
+window.addEventListener("load",loadEvents);
 
-
-window.addEventListener("load",function(){
+function loadEvents(){
     var btn = $("btn");
-    btn.addEventListener("click", operar)
-});
+    btn.addEventListener("click", operar);
+}
 
 function $(id){
-    var element = document.getElementById(id);
-    return element;
+    return document.getElementById(id);
 }
+
+var xhttp = new XMLHttpRequest();
+var servidor = "http://192.168.2.28:3000/loginUsuario";
+var respuestaDelServidor;
 
 function operar(){
     var usr = $("usr").value;
-    var pass = $("1234").value;
-
+    var pass = $("pass").value;
+    //console.log(`${usr} ${pass}`);
     if(usr == "" || pass == "") 
         alert("Faltan datos");
     else{
-        var xhttp = new XMLHttpRequest();
-        var peticion = "?usr="+usr+"&pass="+pass;
+        var peticion = `?usr=${usr}&pass=${pass}`;
         xhttp.onreadystatechange = callback;
-        // xhttp.open("GET","http://192.168.2.28:3000/loginUsuario"+peticion,true); Metodo GET
-        xhttp.open("POST","http://192.168.2.28:3000/loginUsuario",true)
+        xhttp.open("POST", servidor, true)
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhttp.send(peticion);  
-        
+        console.log(peticion);
         function callback(){
             if(xhttp.readyState === 4 && xhttp.status === 200){
-                // console.log("Llego la repuesta del servidor", xhttp.readyState, xhttp.status, xhttp.responseText);
-                var resp = xhttp.responseText;
-                if(resp == "true")
-                    alert("Login ok");
-                else
-                    alert("Error al loguear");
+                respuestaDelServidor = xhttp.responseText;
+                console.log(respuestaDelServidor);
             }   
-            // else
-            //     alert("Error del servidor " + xhttp.status);
         }
     }   
 }
 
-// var str = '{nombre:"Nicolas",apellido:"Aguirre"}';
-// var object = {}; //json - objeto
-// var array = []; //array
-// var persona = {nombre:"Seba",apellido:"Aguirre",edad:"28"}; //json
-// var personas = [{nombre:"Seba",apellido:"Aguirre",edad:"28"},{nombre:"Juan",apellido:"Perez",edad:"30"}] //array de Json
-
-// JSON.parse(str) // transforma un string a objeto json
 
--- Examenes/PracticaPrimerParcial/js/scriptIndex.js
+++ Examenes/PracticaPrimerParcial/js/scriptIndex.js
@@ -7,25 +7,68 @@ window.addEventListener("load", function(){
     $("newPost").addEventListener("click", nuevoPost);
     $("btnCerrar").addEventListener("click", cerrar);
     $("btnPost").addEventListener("click", enviarDatos);
+    // var color = getParameterByName("color", window.url);
+    // var font = getParameterByName("font", window.url);
+    // var email = getParameterByName("email", window.url);
 });
 
+var xhr = new XMLHttpRequest();
+var servidor = "http://localhost:1337/postearNuevaEntrada";
+var respuestaDelServidor;
+
 function nuevoPost(e){
-    e.preventDefault;
     console.log("Nuevo post");
     var oculto = $("oculto");
     oculto.hidden = false;
 }
 
 function cerrar(e){
-    e.preventDefault;
     console.log("Cerrar");
     var oculto = $("oculto");
     oculto.hidden = true;
 }
 
 function enviarDatos(e){
-    e.preventDefault;
-    console.log("Enviar Datos");
+    e.preventDefault();
+    cerrar();
+    mostrarSpinner(false);
+    var textTitle = $("inputTitle").value;
+    var textHeader = $("inputHeader").value;
+    var postText = $("inputText").value;
+    var autor = getParameterByName("email", window.url);
+    //console.log(`Enviar los siguientes datos: ${textTitle} ${textHeader} ${postText}`);
+    
+    var datosPost = {
+        "title":textTitle,
+        "header":textHeader,
+        "posttext":postText,
+        "author":autor
+    }
+
+    xhr.onreadystatechange = callback;
+    xhr.open("POST", servidor, true);
+    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xhr.send(JSON.stringify(datosPost));
+    
+    function callback(){
+        
+        if(xhr.readyState === 4 && xhr.status === 200){
+            respuestaDelServidor = JSON.parse(xhr.responseText);
+            var element = $("posteo");
+            var titulo = document.createElement("h1");
+            var subTitulo = document.createElement("h2");
+            var datos = document.createElement("label");
+            var contenedor = document.createElement("div");
+            titulo.appendChild(document.createTextNode(respuestaDelServidor.title));
+            subTitulo.appendChild(document.createTextNode(respuestaDelServidor.posttext));
+            datos.appendChild(document.createTextNode(`Posted by ${respuestaDelServidor.author} on ${respuestaDelServidor.date}`));
+            contenedor.appendChild(titulo);
+            contenedor.appendChild(subTitulo);
+            contenedor.appendChild(datos);
+            element.appendChild(contenedor);
+            mostrarSpinner(true);
+        }
+    }
 }
 
 function getParameterByName(name, url) {
@@ -37,3 +80,8 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }   
+
+function mostrarSpinner(estado){
+    var spinner = $("cont-spinner");
+    spinner.hidden = estado;
+}
