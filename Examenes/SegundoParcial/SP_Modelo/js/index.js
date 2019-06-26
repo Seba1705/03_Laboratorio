8b41c60c7144cc75b5e8f338f15c6baf5fab2acd
Sebastian1705 <sebaaguirre2012@gmail.com>
2019-06-26 12:54:36 -0300

    Preparando material para el parcial

---
+++ Examenes/SegundoParcial/SP_Modelo/js/index.js
@@ -0,0 +1,34 @@
+"use strict";
+//ARRAY DE ENTIDADES
+var entidades = (localStorage.getItem('entidades') != null) ? JSON.parse(localStorage.getItem('entidades')) : [];
+//GUARDAR EN LOCAL STORAGE
+var guardarEnLocalStorage = function () {
+    localStorage.setItem('entidades', JSON.stringify(entidades));
+};
+//LEER LOCAL STORAGE
+function leerLocalStorage() {
+    var objetos = [];
+    entidades.forEach(function (element) {
+        objetos.push(JSON.parse(element));
+    });
+    return objetos;
+}
+$(function () {
+    $('#btn-modificar').hide();
+    $('#btn-agregar').click(agregar);
+    $('#btn-modificar').click(modificar);
+    $('#btn-eliminar').click(eliminar);
+    $('#btn-cancelar').click(cancelar);
+});
+function agregar() {
+    console.log('Agregar');
+}
+function modificar() {
+    console.log('Modificar');
+}
+function eliminar() {
+    console.log('Eliminar');
+}
+function cancelar() {
+    console.log('Cancelar');
+}
---
+++ Examenes/SegundoParcial/SP_Modelo/ts/index.ts
@@ -0,0 +1,42 @@
+//ARRAY DE ENTIDADES
+let entidades:any[] = (localStorage.getItem('entidades') != null) ? JSON.parse(localStorage.getItem('entidades')) : [];
+
+//GUARDAR EN LOCAL STORAGE
+const guardarEnLocalStorage = () => {
+    localStorage.setItem('entidades', JSON.stringify(entidades));
+}
+
+//LEER LOCAL STORAGE
+function leerLocalStorage():any{
+    let objetos:any[] =  [];
+    entidades.forEach(element =>{
+        objetos.push(JSON.parse(element));
+    });
+    return objetos;
+}
+
+$(function(){
+    $('#btn-modificar').hide();
+
+    $('#btn-agregar').click(agregar);
+    $('#btn-modificar').click(modificar);
+    $('#btn-eliminar').click(eliminar);
+    $('#btn-cancelar').click(cancelar);
+});
+
+
+function agregar(){
+    console.log('Agregar');
+}
+
+function modificar(){
+    console.log('Modificar');
+}
+
+function eliminar(){
+    console.log('Eliminar');
+}
+
+function cancelar(){
+    console.log('Cancelar');
+}
